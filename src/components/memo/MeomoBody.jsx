7b2acc5a6fbc68a0178796416194aa47d49cff20
limfruit <148676716+limfruit@users.noreply.github.com>
2023-12-15 12:04:02 +0900

    memo

--- src/components/memo/MeomoBody.jsx
+++ src/components/memo/MeomoBody.jsx
@@ -3,31 +3,31 @@ import Modal from '../ui/Modal';
 import { createPortal } from 'react-dom';
 import MemoForm from './MemoForm';
 const memos = [
-    {
-      uid: "1",
-      id: "jiholine10",
-      title: '안녕',
-      summary: '안녕한다 얘들아',
-      updateTime: '23.12.14 10:05:23',
-      favorite: false,
-    },
-    {
-      uid:"2",
-      id: "park5606",
-      title: '재밌게 놀사람',
-      summary: '건대로 모여라',
-      updateTime: '23.12.14 10:05:23',
-      favorite: false,
-    },
-    {
-      uid:"3",
-      id: "jihwan",
-      title: '소개 받을분',
-      summary: '커피를 마시고 영화 ㄱㄱ',
-      updateTime: '23.12.14 10:05:23',
-      favorite: false,
-    }
-  ];
+  {
+    uid: "1",
+    id: "jiholine10",
+    title: '안녕',
+    summary: '안녕한다 얘들아',
+    updateTime: '23.12.14 10:05:23',
+    favorite: false,
+  },
+  {
+    uid: "2",
+    id: "park5606",
+    title: '재밌게 놀사람',
+    summary: '건대로 모여라',
+    updateTime: '23.12.14 10:05:23',
+    favorite: false,
+  },
+  {
+    uid: "3",
+    id: "jihwan",
+    title: '소개 받을분',
+    summary: '커피를 마시고 영화 ㄱㄱ',
+    updateTime: '23.12.14 10:05:23',
+    favorite: false,
+  }
+];
 const MeomoBody = () => {
   const [plusmemo, setMemo] = useState(memos);
   const addMemoHandler = ({ id, title, summary }) => {
@@ -96,15 +96,15 @@ const MeomoBody = () => {
             plusmemo.map(memo =>
               <td className='cell' key={memo.uid}>
                 <div className='inner'>
-                <button onClick={()=>{starClick(memo.uid)}} className={returnFavClass(memo.favorite)}>{returnStar(memo.favorite)}</button>
-                <div onClick={() => { openModal(memo.uid) }}>
-                   <h5 > {memo.title} </h5>
-                  <div className='container right-fix user-id'><div> {memo.id} </div></div>
-                  <h4 className="text-ellipsis overflow-hidden ..."> {memo.summary} </h4>
-                  <h1>{memo.updateTime}</h1>
+                  <button onClick={() => { starClick(memo.uid) }} className={returnFavClass(memo.favorite)}>{returnStar(memo.favorite)}</button>
+                  <div onClick={() => { openModal(memo.uid) }}>
+                    <div classNmae='title'> {memo.title} </div>
+                    <h4 className="summary text-ellipsis overflow-hidden ..."> {memo.summary} </h4>
+                    <div className='container right-fix user-id'><div className='time'> {memo.id} </div></div>
+                    <div className='container right-fix time'>{memo.updateTime}</div>
                   </div>
                 </div>
-               
+
                 {isOpen && createPortal(
                   <Modal onClose={closeModal}>
                     <MemoForm memo={plusmemo[curUid - 1]} onAdd={addMemoHandler} onClose={closeModal}>#</MemoForm>
